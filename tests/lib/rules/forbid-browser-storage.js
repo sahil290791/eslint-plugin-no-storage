5708a03de121505ead0567007b9e7b1a7e096872
Tzach Yarimi <tzach@implisit.com>
2016-02-22 10:22:00 +0200

    adding rule code

--- tests/lib/rules/forbid-browser-storage.js
+++ tests/lib/rules/forbid-browser-storage.js
@@ -12,7 +12,7 @@
 
 var rule = require("../../../lib/rules/forbid-browser-storage"),
 
-    RuleTester = require("eslint").RuleTester;
+	RuleTester = require("eslint").RuleTester;
 
 
 //------------------------------------------------------------------------------
@@ -22,18 +22,52 @@ var rule = require("../../../lib/rules/forbid-browser-storage"),
 var ruleTester = new RuleTester();
 ruleTester.run("forbid-browser-storage", rule, {
 
-    valid: [
-
-        // give me some code that won't trigger a warning
+	valid: [
+        "SessionStorage.x = 1;",
+        "LocalStorage.x = 1;",
+        "Storage.x = 1;"
     ],
 
-    invalid: [
-        {
-            code: "sessionStorage.setItem('hello', 'world')",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
+	invalid: [
+		{
+			code: "window.sessionStorage.setItem('x', 'y')",
+			errors: [{message: "Unexpected use of sessionStorage.", type: "Identifier"}]
+		},
+		{
+			code: "sessionStorage.setItem('x', 'y')",
+			errors: [{message: "Unexpected use of sessionStorage.", type: "Identifier"}]
+		},
+		{
+			code: "sessionStorage['x'] == 1;",
+			errors: [{message: "Unexpected use of sessionStorage.", type: "Identifier"}]
+		},
+		{
+			code: "sessionStorage.a == 1;",
+			errors: [{message: "Unexpected use of sessionStorage.", type: "Identifier"}]
+		},
+		{
+			code: "sessionStorage.a = 1;",
+			errors: [{message: "Unexpected use of sessionStorage.", type: "Identifier"}]
+		},
+		{
+			code: "window.localStorage.setItem('x', 'y')",
+			errors: [{message: "Unexpected use of localStorage.", type: "Identifier"}]
+		},
+		{
+			code: "localStorage.setItem('x', 'y')",
+			errors: [{message: "Unexpected use of localStorage.", type: "Identifier"}]
+		},
+		{
+			code: "localStorage['x'] == 1;",
+			errors: [{message: "Unexpected use of localStorage.", type: "Identifier"}]
+		},
+		{
+			code: "localStorage.a == 1;",
+			errors: [{message: "Unexpected use of localStorage.", type: "Identifier"}]
+		},
+		{
+			code: "localStorage.a = 1;",
+			errors: [{message: "Unexpected use of localStorage.", type: "Identifier"}]
+		}
+	]
 });
